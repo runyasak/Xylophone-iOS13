ce03e3379997f974f661d5a05eb6f985e0238474
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-28 21:36:26 +0700

    feat: implement PlaytSound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("I Got Pressed")
+        playSound()
     }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
 
+        if let safePlayer = player {
+            safePlayer.play()
+        }
+    }
+    
 }
 
